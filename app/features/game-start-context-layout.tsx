d8afe2000c7c84bbcde810d4b8562b510751546a
Willis Kim <kshwan.1023@gmail.com>
2025-07-10 13:09:57 +0900

    cloudflare upload

--- app/features/game-start-context-layout.tsx
+++ app/features/game-start-context-layout.tsx
@@ -3,7 +3,7 @@ import { GameStartContextProvider } from "./home/game-start-context";
 
 export default function GameStartContextLayout() {
   return (
-    <GameStartContextProvider initValue={{ gameIndex: 0, selectedGames: [] }}>
+    <GameStartContextProvider>
       <Outlet />
     </GameStartContextProvider>
   );
